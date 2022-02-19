fa07a495b859c32ece060774bd612209b4f8bc76
Marica Bogdan <marica.bogdann@gmail.com>
2022-02-20 00:38:16 +0200

    First

---
+++ src/components/NoutFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const NoutFound = () => {
+  return (
+    <div className='md:container md:mx-auto text-center justify-center items-center'>
+        <h1>OPPS!</h1>
+        <div className="flex flex-wrap justify-center">
+            <h1>404 ERROR</h1>
+            <button></button>
+        </div>
+    </div>
+  )
+}
